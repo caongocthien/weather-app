3872e5091932b0a07125e9dd7ba15829f14dc5f8
Thien Cao <caongocthien36@gmail.com>
2023-04-15 02:11:52 +0700

    change icon belong to weather

---
+++ src/constants/weather.constant.ts
@@ -0,0 +1,244 @@
+const weather = [
+  {
+    name: 'Sunny',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Partly cloudy',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Cloudy',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Mist',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy rain possible',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy snow possible',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy sleet possible',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy freezing drizzle possible',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Thundery outbreaks possible',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Blowing snow',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Blizzard',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Fog',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Freezing fog',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy light drizzle',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light drizzle',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Freezing drizzle',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Heavy freezing drizzle',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy light rain',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light rain',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate rain at times',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate rain',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Heavy rain at times',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Heavy rain',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light freezing rain',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy freezing rain',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light sleet',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy sleet',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy light snow',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light snow',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy moderate snow',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate snow',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy heavy snow',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Heavy snow',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Ice pellets',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light rain shower',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy rain shower',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Torrential rain shower',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light sleet showers',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy sleet showers',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light snow showers',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy snow showers',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Light showers of ice pellets',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy showers of ice pellets',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy light rain with thunder',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy rain with thunder',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Patchy light snow with thunder',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Moderate or heavy snow with thunder',
+    day: '',
+    night: ''
+  },
+  {
+    name: 'Clear',
+    day: '',
+    night: ''
+  }
+]
+export default weather
+console.log(weather.length)
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { useSearchParams } from 'react-router-dom'
+import lookup from 'country-code-lookup'
 
 export const useQueryParams = () => {
   const [searchParams] = useSearchParams()
@@ -8,7 +9,36 @@ export const useQueryParams = () => {
 }
 
 export const convertDatetoDay = (date: string) => {
-  const weekday = ['Sun', 'Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat']
+  const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
   const initDate = new Date(date)
   return weekday[initDate.getDay()]
+}
+
+interface Location {
+  latitude: string
+  longitude: string
+}
+
+export const getLocation = <T extends Location>(): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const latitude = position.coords.latitude.toString()
+        const longitude = position.coords.longitude.toString()
+        const location = { latitude: latitude, longitude: longitude } as T
+        resolve(location)
+      })
+    } else {
+      reject('Geolocation is not supported by this browser.')
+    }
+  })
+}
+
+export const getCountryCodeIso2 = (contryName: string) => {
+  const result = lookup.byCountry(contryName as string)
+  return result?.iso2.toLocaleLowerCase()
+}
+
+export const getSessionInDayByUrl = (url: string) => {
+  return url && url.split('/')[5]
 }
